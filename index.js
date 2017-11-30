a1de7dad6701b0fb9316b376167064c559bb4fbf
Thomas Crescenzi <trescenzi@gmail.com>
2017-11-30 15:06:21 -0500

    feat(clubhouse): add clubhouse question to commitizen prompt
    The majority of this code comes from
    [cz-conventional-changelog](https://github.com/commitizen/cz-conventional-changelog).
    I've added the clubhouse question along with sprucing up the code a bit to be
    more modern (const () => and such).

---
+++ index.js
@@ -0,0 +1,126 @@
+const wrap = require('word-wrap');
+const map = require('lodash.map');
+const longest = require('longest');
+const rightPad = require('right-pad');
+const types = require('conventional-commit-types').types;
+
+const STORY_REGEX = /[\s,.;]/;
+
+function filter(array) {
+  return array.filter(function(x) {
+    return x;
+  });
+};
+
+function formatTypes(types) {
+  const length = longest(Object.keys(types)).length + 1;
+  return choices = map(types, function (type, key) {
+    return {
+      name: rightPad(key + ':', length) + ' ' + type.description,
+      value: key
+    };
+  });
+}
+
+function formatStory(story) {
+  return `[ch${story}]`;
+}
+
+module.exports = {
+  // When a user runs `git cz`, prompter will
+  // be executed. We pass you cz, which currently
+  // is just an instance of inquirer.js. Using
+  // this you can ask questions and get answers.
+  //
+  // The commit callback should be executed when
+  // you're ready to send back a commit template
+  // to git.
+  prompter: function(cz, commit) {
+    console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+
+    cz.prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Select the type of change that you\'re committing:',
+        choices: formatTypes(types),
+      }, {
+        type: 'input',
+        name: 'scope',
+        message: 'What is the scope of this change? (press enter to skip)'
+      }, {
+        type: 'input',
+        name: 'subject',
+        message: 'Write a short, imperative tense description of the change:'
+      }, {
+        type: 'input',
+        name: 'body',
+        message: 'Provide a longer description of the change: (press enter to skip)\n'
+      }, {
+        type: 'confirm',
+        name: 'isBreaking',
+        message: 'Are there any breaking changes?',
+        default: false
+      }, {
+        type: 'input',
+        name: 'breaking',
+        message: 'Describe the breaking changes:\n',
+        when: (answers) => answers.isBreaking,
+      }, {
+        type: 'confirm',
+        name: 'isIssueAffected',
+        message: 'Does this change affect any open Github issues?',
+        default: false
+      }, {
+        type: 'input',
+        name: 'issues',
+        message: 'Add issue references (e.g. "fix #123", "re #123".):',
+        when: (answers) => answers.isIssueAffected,
+      }, {
+        type: 'confirm',
+        name: 'isClubhouseStory',
+        message: `Would you like to directly reference any Clubhouse.io stories that aren't already linked via the branch?`,
+        default: false,
+      }, {
+        type: 'input',
+        name: 'stories',
+        message: `What stories?`,
+        when: (answers) => answers.isClubhouseStory,
+      }
+    ]).then(function(answers) {
+
+      var maxLineWidth = 100;
+
+      var wrapOptions = {
+        trim: true,
+        newline: '\n',
+        indent:'',
+        width: maxLineWidth
+      };
+
+      // parentheses are only needed when a scope is present
+      let scope = answers.scope.trim();
+      scope = scope ? '(' + answers.scope.trim() + ')' : '';
+
+      // Hard limit this line
+      const head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+
+      // Wrap these lines at 100 characters
+      const body = wrap(answers.body, wrapOptions);
+
+      // Apply breaking change prefix, removing it if already present
+      let breaking = answers.breaking ? answers.breaking.trim() : '';
+      breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
+      breaking = wrap(breaking, wrapOptions);
+
+      const issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
+
+      console.log(answers.stories);
+      const stories = answers.stories ? answers.stories.split(STORY_REGEX).map(formatStory).join('\n') : '';
+
+      const footer = filter([ stories, breaking, issues ]).join('\n\n');
+
+      commit(head + '\n\n' + body + '\n\n' + footer);
+    });
+  }
+};
